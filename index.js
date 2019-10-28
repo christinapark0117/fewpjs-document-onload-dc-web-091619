2c3970b6d9e21b0d149ad1f3c41b5a343ef18988
github username <github email address>
2019-10-28 16:43:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    updateDom();
+});
+
+function updateDom() {
+   let textElm = document.getElementById("text")
+    textElm.textContent = "This is really cool!"
+}
